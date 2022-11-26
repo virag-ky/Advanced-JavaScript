61993d1603477bff07f9ebd521e5b1c6de6ef2b6
Virag Kormoczy <vkormoczy@gmail.com>
2022-11-26 21:25:41 +0200

    Add currying example

--- currying/currying.js
+++ currying/currying.js
@@ -1,4 +1,6 @@
-/* The idea of currying is when we take a function with multiple arguments and we transform that function into a sequence of nesting functions that take one argument at a time. */
+/* The idea of currying is when we take a function with multiple arguments
+ and we transform that function into a sequence of nesting functions 
+ that take one argument at a time. */
 
 function sum(a, b, c) {
   return a + b + c;
@@ -32,6 +34,7 @@ sum2(2)(3)(5); // 10
 
 /* Why would we need currying?
 
-Currying is a checking method to make sure that you get everything you need before you proceed. It helps you to avoid passing the same variable again and again. It divides your function into multiple smaller functions that can handle one responsibility.
-
+Currying is a checking method to make sure that you get everything you need before you proceed.
+It helps you to avoid passing the same variable again and again. 
+It divides your function into multiple smaller functions that can handle one responsibility.
 */
